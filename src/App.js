072de6c06ffbdd5e77a13207e280dab6f746b021
styrene <waghelaharsh56@gmail.com>
2022-08-13 22:08:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div class="column navigation">
+                <h2>Spotifty</h2>
+            </div>
+            <div class="column for-you">
+                <h2>For You</h2>
+            </div>
+            <div class="column music-details">
+                <h2>Viva la vida</h2>
+            </div>
+        </div>
+    );
 }
 
 export default App;
